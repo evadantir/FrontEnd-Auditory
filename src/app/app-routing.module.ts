4da403d9f42c088fc7309b65ed0bd425ab8856d6
BERPERGIAN\Berpergian <ganjar_ibrahimovic@rocketmail.com>
2017-05-29 04:32:31 +0700

    add album and category to module and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './component/app.component';
 import { StreamComponent } from './component/stream.component';
+import { AudioListComponent } from './component/audiolist.component';
+import { CategoryComponent } from './component/category.component';
+import { AlbumComponent } from './component/album.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: AppComponent },
-  { path: 'stream', component: StreamComponent }
+  { path: 'stream', component: StreamComponent },
+  { path: 'list', component: AudioListComponent },
+  { path: 'category', component: CategoryComponent },
+  { path: 'album', component: AlbumComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,33 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './component/app.component';
 import { StreamComponent }  from './component/stream.component';
+import { AudioListComponent } from './component/audiolist.component';
+import { CategoryComponent } from './component/category.component';
+import { AlbumComponent } from './component/album.component';
+
+import { AudioService } from './service/audio.service';
+import { AlbumService } from './service/album.service';
+import { CategoryService } from './service/category.service';
 
 @NgModule({
-  imports:      [ BrowserModule, AppRoutingModule ],
-  declarations: [ AppComponent, StreamComponent ],
+  imports:      [ BrowserModule, HttpModule, FormsModule, AppRoutingModule ],
+  declarations: [
+    AppComponent,
+    StreamComponent,
+    AudioListComponent,
+    CategoryComponent,
+    AlbumComponent
+  ],
+  providers: [
+    AudioService,
+    AlbumService,
+    CategoryService
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
